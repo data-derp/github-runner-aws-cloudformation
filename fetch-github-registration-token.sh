51804fd8db64ce293f344e17eda20f79b42eb595
Kelsey Mok <kelseymok@gmail.com>
2022-01-11 14:03:40 +0100

    km: handling directory-ception

--- fetch-github-registration-token.sh
+++ fetch-github-registration-token.sh
@@ -10,6 +10,7 @@ fetch-repo-name() {
 
 fetch-github-registration-token() {
   github_username="${1}"
+  github_repo_name="${2}"
 
   if [ -z "${github_username}" ]; then
     echo "GITHUB_USERNAME not set. Usage <func> GITHUB_USERNAME"
@@ -20,7 +21,7 @@ fetch-github-registration-token() {
     -u $github_username \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/$(fetch-repo-name)/actions/runners/registration-token)
+    https://api.github.com/repos/${github_repo_name}/actions/runners/registration-token)
 
   echo $response | jq -r .token
 }
